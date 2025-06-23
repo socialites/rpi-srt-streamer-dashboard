8b9d9e4722424d2c742378c3918515d984d816fe
chukwumaokere <chukwumaokere@yahoo.com>
2025-06-22 23:04:42 -0500

    update: test mjpeg

--- src/components/StreamPreview.tsx
+++ src/components/StreamPreview.tsx
@@ -1,36 +1,11 @@
-import mpegts from 'mpegts.js';
-import { useEffect, useRef } from 'preact/hooks';
-
 export default function StreamPreview() {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
-  useEffect(() => {
-    if (mpegts.getFeatureList().mseLivePlayback && videoRef.current) {
-      const player = mpegts.createPlayer({
-        type: 'mpegts',
-        isLive: true,
-        url: `http://${window.location.hostname}:8080/preview`,
-      });
-
-      player.attachMediaElement(videoRef.current);
-      player.load();
-      player.play();
-
-      return () => {
-        player.unload();
-        player.detachMediaElement();
-        player.destroy();
-      };
-    }
-  }, []);
-
-  return (
-    <video
-      ref={videoRef}
-      controls
-      autoPlay
-      muted
-      className="w-full max-w-640px"
-    />
-  );
-}
+    return (
+      <div class="rounded-md overflow-hidden border border-white/10">
+        <img
+          src={`http://${window.location.hostname}/preview`}
+          alt="Live Preview"
+          className="w-full max-w-full"
+        />
+      </div>
+    )
+  }
