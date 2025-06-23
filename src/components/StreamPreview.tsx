6faa03534265d64815daf3d185601b856988d975
chukwumaokere <chukwumaokere@yahoo.com>
2025-06-22 23:38:17 -0500

    update: change to using hls

--- src/components/StreamPreview.tsx
+++ src/components/StreamPreview.tsx
@@ -1,11 +1,28 @@
+import Hls from 'hls.js';
+import { useEffect, useRef } from 'preact/hooks';
+
 export default function StreamPreview() {
-    return (
-      <div class="rounded-md overflow-hidden border border-white/10">
-        <img
-          src={`http://${window.location.hostname}/preview`}
-          alt="Live Preview"
-          className="w-full max-w-full"
-        />
-      </div>
-    )
-  }
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current && Hls.isSupported()) {
+      const hls = new Hls();
+      hls.loadSource(`http://${window.location.hostname}/hls/preview.m3u8`);
+      hls.attachMedia(videoRef.current);
+      return () => hls.destroy();
+    }
+  }, []);
+
+  return (
+    <div class="rounded-md overflow-hidden border border-white/10">
+      <video
+        ref={videoRef}
+        className="w-full max-w-full"
+        autoPlay
+        muted
+        playsInline
+        controls
+      />
+    </div>
+  );
+}
