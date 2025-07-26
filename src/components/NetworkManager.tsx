b820a8f075e76e7400a2f2a2607628038cf7c5e3
chukwumaokere <chukwumaokere@yahoo.com>
2025-07-26 00:39:48 -0500

    add icons

--- src/components/NetworkManager.tsx
+++ src/components/NetworkManager.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import { RotateCcw, Signal, SignalHigh, SignalLow, SignalMedium, SignalZero, Trash2 } from "lucide-react";
 import { useCallback, useEffect, useState } from "preact/hooks";
 import { toast, type ToastContentProps } from 'react-toastify';
 import { connectToNetwork, disconnectFromNetwork, forgetNetwork, getNetworks } from "../apis";
@@ -9,6 +10,14 @@ import { SplitButtons } from './SplitButtons';
 const connectPromptId = 'connect-to-network-prompt';
 const disconnectPromptId = 'disconnect-from-network-prompt';
 const forgetNetworkPromptId = 'forget-network-prompt';
+
+const SignalIcon = ({ signal, className }: { signal: number, className?: string }) => {
+    if (signal >= 90) return <Signal className={className} />;
+    if (signal > 75) return <SignalHigh className={className} />;
+    if (signal > 50) return <SignalMedium className={className} />;
+    if (signal > 25) return <SignalLow className={className} />;
+    return <SignalZero className={className} />;
+};
 
 export function NetworkManager({ toggleNetworkManager }: { toggleNetworkManager: () => void }) {
     const { data: networks, isError, error, refetch } = useQuery({
@@ -61,6 +70,7 @@ export function NetworkManager({ toggleNetworkManager }: { toggleNetworkManager:
         }
     }, [isError, error]);
 
+
     return (
         <Modal onPress={toggleNetworkManager}>
             <div class="bg-slate-800 p-4 rounded-md w-full h-full flex flex-col gap-2 text-white z-10 xl:max-w-lg xl:max-h-1/2 max-h-5/6" onClick={e => e.stopPropagation()}>
@@ -74,14 +84,14 @@ export function NetworkManager({ toggleNetworkManager }: { toggleNetworkManager:
                                     <p>{network.ssid} {network.in_use && '(Connected)'}</p>
                                     <div class="flex flex-row gap-2 text-xs">
                                         <p>Speed: {network.rate}</p>
-                                        <p>Signal Strength: {network.signal}</p>
+                                        <p>Signal Strength: <SignalIcon signal={network.signal} className="w-4 h-4" /></p>
                                         <p>{network.security}</p>
                                     </div>
                                 </div>
                             </Button>
                             {network.in_use && (
                                 <div class="absolute top-0 right-0 p-2">
-                                    <Button className="bg-red-500 w-4 h-4 text-white text-xs !p-3 flex items-center justify-center rounded-md" onClick={() => promptForgetNetwork(network.ssid)}>🗑️</Button>
+                                    <Button className="bg-red-500 w-4 h-4 text-white text-xs !p-3 flex items-center justify-center rounded-md" onClick={() => promptForgetNetwork(network.ssid)}><Trash2 /></Button>
                                 </div>
                             )}
                         </div>
@@ -93,7 +103,7 @@ export function NetworkManager({ toggleNetworkManager }: { toggleNetworkManager:
                     }
                 </div>
                 <div class="flex flex-col gap-2">
-                    <Button className="bg-yellow-500 text-white p-2 rounded-md" onClick={() => refetch()}>Re-scan</Button>
+                    <Button className="bg-yellow-500 text-white p-2 rounded-md flex items-center justify-center gap-2" onClick={() => refetch()}><RotateCcw className="w-4 h-4" /> Re-scan</Button>
                     <Button className="bg-red-500 text-white p-2 rounded-md" onClick={() => toggleNetworkManager()}>Cancel</Button>
                 </div>
             </div>
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
 
-export function Button({ children, onClick, className, disabled }: { children: preact.JSX.Element | string, onClick: () => void, className?: string, disabled?: boolean }) {
+export function Button({ children, onClick, className, disabled }: { children: (preact.JSX.Element | Element | string) | (preact.JSX.Element | Element | string)[], onClick: () => void, className?: string, disabled?: boolean }) {
   return <button class={classNames('text-white p-3 rounded-md', className)} onClick={onClick} disabled={disabled}>{children}</button>;
 }
