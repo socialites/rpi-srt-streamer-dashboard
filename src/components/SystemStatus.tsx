9cd5277c8ac4c68737390661680d89b691d85ab5
chukwumaokere <chukwumaokere@yahoo.com>
2025-07-28 22:21:30 -0500

    filter out IPv6

--- src/components/SystemStatus.tsx
+++ src/components/SystemStatus.tsx
@@ -8,6 +8,10 @@ export function SystemStatus({ systemStatus, isError, screen }: { systemStatus: 
 
     const [showApPassword, setShowApPassword] = useState(false);
 
+    const isIPv6 = useCallback((ip: string): boolean => {
+        return ip.includes(':');
+    }, []);
+
     const toggleApPassword = useCallback(() => {
         if (systemStatus?.ap_password && systemStatus.ap_password !== 'not available') {
             setShowApPassword(!showApPassword);
@@ -50,7 +54,7 @@ export function SystemStatus({ systemStatus, isError, screen }: { systemStatus: 
             })}>{systemStatus?.hostname.toUpperCase() || window.location.hostname.toUpperCase()}</h1>
             {isError || systemStatus === undefined ? <p class="text-center text-sm text-red-500">Error occurred while fetching system status. See below for more details.</p> : (
                 <>
-                    <p class="font-mono text-sm flex flex-col flex-wrap gap-1 text-center"><strong>IP(s):</strong> {systemStatus?.ip.split(' ').map(ip => <span class="font-mono text-sm">{ip}</span>)}</p>
+                    <p class="font-mono text-sm flex flex-col flex-wrap gap-1 text-center"><strong>IP(s):</strong> {systemStatus?.ip.split(' ').filter(ip => !isIPv6(ip)).map(ip => <span class="font-mono text-sm">{ip}</span>)}</p>
                     <p><strong>Network Watcher Status:</strong> <span class={classNames("font-mono text-sm", systemStatus?.network_watcher === "active" ? "text-green-500" : systemStatus?.network_watcher === 'Fetching...' ? "text-yellow-500" : "text-red-500")}>{systemStatus?.network_watcher.toUpperCase()}</span></p>
                     <p><strong>SRT Streamer Status:</strong> <span class={classNames("font-mono text-sm", systemStatus?.srt_streamer === "active" ? "text-green-500" : systemStatus?.srt_streamer === 'Fetching...' ? "text-yellow-500" : "text-red-500")}>{systemStatus?.srt_streamer.toUpperCase()}</span></p>
                     <p><strong>Access Point Status:</strong> <span class={classNames("font-mono text-sm", systemStatus?.ap_status === "up" ? "text-green-500" : systemStatus?.ap_status === 'Fetching...' ? "text-yellow-500" : "text-red-500")}>{systemStatus?.ap_status.toUpperCase()}</span></p>
